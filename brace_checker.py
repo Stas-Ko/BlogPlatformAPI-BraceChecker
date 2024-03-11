2a0c1664a047fc1daabf5571d18d43798887f0a0
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2024-03-11 19:32:08 +0200

    Update brace_checker.py

--- brace_checker.py
+++ brace_checker.py
@@ -8,11 +8,11 @@ def is_valid_parentheses(s):
             if stack and stack[-1][0] == mapping[char]:
                 stack.pop()
             else:
-                return False, i  # Возвращает False и индекс неправильной скобки
+                return False, i  # Returns False and the index of the incorrect parenthesis
     if stack:
-        return False, stack[0][1]  # Возвращает индекс первой непарной скобки
-    return True, -1  # Все скобки правильные
+        return False, stack[0][1]  # Returns the index of the first unpaired parenthesis
+    return True, -1  # All parentheses are correct
 
-# Пример использования функции
+# Example of using the function
 print(is_valid_parentheses("{[]}"))  # (True, -1)
 print(is_valid_parentheses("{[}]"))  # (False, 2)
